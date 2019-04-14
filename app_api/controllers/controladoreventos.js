7a733486bffcf36448c2b1b9ee997ea608f87c13
Pepe Obando <jsobando96@hotmail.com>
2019-04-14 15:32:19 -0500

    NiIdeaQueHice

---
+++ app_api/controllers/controladoreventos.js
@@ -0,0 +1,92 @@
+var mongoose = require('mongoose');
+var Eve = mongoose.model('Evento');
+
+var sendJsonResponse = function(res, status, content){
+	res.status(status);
+	res.json(content);
+}
+
+module.exports.EventosCreate = function (req, res) {
+  console.log(req.body);
+  Eve.create({
+    nombreevento: req.body.nombre,
+    fecha: req.body.profesion,
+    foto: req.body.foto
+
+  }, function(err,evento){
+    if(err){
+      console.log(err);
+      sendJsonResponse(res,400,err);
+    }else{
+      console.log(evento);
+      sendJsonResponse(res,201,evento);
+    }
+  }
+
+  )};
+
+module.exports.EventosList = function (req, res) {
+  Eve.find().exec(
+      function(err, evento){
+        if (!evento) {
+          sendJsonResponse(res, 404, {
+            "message": "id not found"
+          });
+          return;
+        } else if (err) {
+          sendJsonResponse(res, 400, err);
+          return;
+        }
+        sendJsonResponse(res, 200, evento);
+      });
+  };
+
+
+
+
+module.exports.EventosDeleteOne = function (req, res) {
+  var id = req.params.eventosid;
+  if (id) {
+    Eve
+    .findByIdAndRemove(id)
+    .exec(
+      function(err, evento) {
+      if (err) {
+        console.log(err);
+        sendJsonResponse(res, 404, err);
+        return;
+      }
+      console.log("Deleted");
+      sendJsonResponse(res, 204, null);
+      }
+    );
+  } else {
+    sendJsonResponse(res, 404, {
+    "message": "No id"
+    });
+  }
+  };
+
+  module.exports.EventosReadOne = function(req, res){
+    if (req.params && req.params.artistasid){
+      Eve
+        .findById(req.params.eventosid)
+        .exec(function(err, evento){
+          if(!evento){
+            console.log(req.params.eventosid);
+            sendJsonResponse(res, 404, {
+              "message": "id not found"
+            });
+            return;
+          } else if(err){
+            sendJsonResponse(res, 404, err);
+            return;
+          }
+          sendJsonResponse(res, 200, evento);
+        });
+    }else{
+      sendJsonResponse(res, 404, {
+        "message": "No id in request"
+      });
+    }
+  };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,15 +2,15 @@
 var express = require ('express');
 var router = express.Router();
 var ctrlArtistas = require('../controllers/controlador');
-
+var ctrlEventos = require('../controllers/controladoreventos');
 
 router.get('/artistas', ctrlArtistas.ArtistasList);
 router.post('/artistas', ctrlArtistas.ArtistasCreate);
 router.get('/artistas/:artistasid', ctrlArtistas.ArtistasReadOne);
 router.delete('/artistas/:artistasid',ctrlArtistas.ArtistasDeleteOne);
 
-router.get('/eventos', ctrlArtistas.EventosList);
-router.post('/eventos', ctrlArtistas.EventosCreate);
-router.get('/eventos/:eventosid', ctrlArtistas.EventosReadOne);
-router.delete('/eventos/:eventosid',ctrlArtistas.EventosDeleteOne);
+router.get('/eventos', ctrlEventos.EventosList);
+router.post('/eventos', ctrlEventos.EventosCreate);
+router.get('/eventos/:eventosid', ctrlEventos.EventosReadOne);
+router.delete('/eventos/:eventosid',ctrlEventos.EventosDeleteOne);
 module.exports = router;
